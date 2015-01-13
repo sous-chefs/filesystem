acf98b272925bf54dca6ab6fe254260ad6062939
Kyle McGovern <kyle.mcgovern@cerner.com>
2015-01-13 16:51:39 -0600

    Make lvm cookbook dependency less pessimistic

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'cookbooks.atrullmdsol@trull.org'
 description      'Installs/Configures various filesystems'
 license          'Apache 2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.9.0"
+version          "0.9.1"
 
 %w/redhat centos xenserver ubuntu debian scientific amazon/.each do |os|
   supports os
 end
 
-depends 'lvm', '~> 1.1.0'
+depends 'lvm', '~> 1.1'
 
 attribute 'filesystems',
   :description => "Filesystems to be created and/or mounted",
