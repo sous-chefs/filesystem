004ce94ec1b08535b67717487a01130c6473adfb
Alex Trull <git.alex@trull.org>
2015-10-13 17:49:41 +0100

    Bumping cookbook version for new release.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks.atrullmdsol@trull.org'
 description      'Installs/Configures various filesystems'
 license          'Apache 2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.10.1"
+version          "0.10.2"
 
 %w/redhat centos xenserver ubuntu debian scientific amazon/.each do |os|
   supports os
