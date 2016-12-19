6fd4529de2a72b4341bf142e0385a1375583652e
Richard Shade <rshade@rightscale.com>
2016-12-19 12:46:43 -0600

    updating Berks and metadata for lvm 3.0

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email 'cookbooks.atrullmdsol@trull.org'
 description      'Installs/Configures various filesystems'
 license          'Apache 2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.8.3"
+version          "0.11.0"
 
 %w/redhat centos xenserver ubuntu debian scientific amazon/.each do |os|
   supports os
 end
 
 depends 'xfs'
-depends 'lvm', '~> 1.1.0'
+depends 'lvm', '~> 3.1'
 
 attribute 'filesystems',
   :description => "Filesystems to be created and/or mounted",
