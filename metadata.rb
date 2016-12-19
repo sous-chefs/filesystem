62091b222e430d268b9f1b7cd50cef2f13fae574
Richard Shade <rshade@rightscale.com>
2016-12-19 12:55:49 -0600

    updating to chef12

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.11.0"
 source_url       "https://github.com/atrull/filesystem_cookbook"
 issues_url       "https://github.com/atrull/filesystem_cookbook/issues"
+chef_version '>= 12.0' if respond_to?(:chef_version)
 
 %w/redhat centos xenserver ubuntu debian scientific amazon/.each do |os|
   supports os
