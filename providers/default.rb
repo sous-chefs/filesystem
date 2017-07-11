2fa6b2057c1fcb8e714ec3e7896463271c330b1d
Lewis <lewisd01@ad.sjm.com>
2017-07-11 14:42:42 -0700

    Updated default provider to notify directory resource immediately upon mounting filesystem.

--- providers/default.rb
+++ providers/default.rb
@@ -230,6 +230,7 @@ action :enable do
       options options
       action :enable
       only_if "test -b #{device}"
+      notifies :create, "directory[#{mount}]", :immediately
     end
 
   end
@@ -275,6 +276,7 @@ action :mount do
       action :mount
       only_if "test -b #{device}"
       not_if "mount | grep #{device}\" \" | grep #{mount}\" \""
+      notifies :create, "directory[#{mount}]", :immediately
     end
 
   end
