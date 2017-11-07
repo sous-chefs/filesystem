a10ad81eadb598c8ae4f906f445bf9258cf210f7
Jordan Neufeld <jordan.neufeld@nfl.com>
2017-11-07 09:40:36 -0600

    Remove package xfsprogs-devel on rhel, fedora
    xfsprogs-devel is no longer available on Redhat 7. The xfsprogs package alone should be sufficient to manage xfs filesystems.

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -14,3 +14,20 @@ describe mount('/mnt/loop-1') do
   its('device') { should eq '/dev/loop0' }
   its('type') { should eq 'ext3' }
 end
+
+describe file('/opt/loop-xfs.img') do
+  it { should be_file }
+  it { should exist }
+  its('size') { should > 1 }
+end
+
+describe directory('/mnt/loop-xfs') do
+  it { should exist }
+  it { should be_directory }
+end
+
+describe mount('/mnt/loop-xfs') do
+  it { should be_mounted }
+  its('device') { should eq '/dev/loop1' }
+  its('type') { should eq 'xfs' }
+end
