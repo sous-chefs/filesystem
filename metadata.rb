27972ae5cc32312fa31061387a0f19b562703705
Jason Field <jason@avon-lea.co.uk>
2020-05-05 20:23:45 +0100

    Release 2.0.0

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,10 @@ maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 description      'Installs/Configures various filesystems'
 license          'Apache-2.0'
-version          '1.0.0'
+version          '2.0.0'
 source_url       'https://github.com/sous-chefs/filesystem'
 issues_url       'https://github.com/sous-chefs/filesystem/issues'
-chef_version     '>= 13'
+chef_version     '>= 14'
 
 %w(redhat centos xenserver ubuntu debian scientific amazon).each do |os|
   supports os
