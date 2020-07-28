371227d2cfe7642e4eceb105c3bd65ff6ec0a1ae
MarkGibbons <gibbonsmj80@gmail.com>
2020-07-27 19:38:05 -0700

    Stop trying to create and nfs share from inside a container.
    NFS shares are not the point of this cookbook and containers don't allow NFS shares easily enough.

--- test/cookbooks/test/recipes/nfs.rb
+++ test/cookbooks/test/recipes/nfs.rb
@@ -8,27 +8,6 @@ return unless platform_family?('debian')
 # apt update
 apt_update 'Update packages'
 
-# apt install nfs-kernel-server
-package 'nfs-kernel-server'
-
-# create a directory
-directory '/exports/nfs1' do
-  recursive true
-end
-
-# configure to export the directory
-file '/etc/exports' do
-  content '/exports/nfs1 *(rw,no_root_squash,no_subtree_check)'
-end
-
-execute 'exportfs -a'
-
-# start the nfs server service
-service 'nfs-kernel-server' do
-  action [:enable, :start]
-  ignore_failure true
-end
-
 filesystem 'nfs-1' do
   fstype 'nfs'
   device 'localhost:/exports/nfs1'
