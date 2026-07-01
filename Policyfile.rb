f1fe25595533bdc3cfd62907510339a70b93ccf7
Dan Webb <dan.webb@damacus.io>
2026-07-01 10:46:35 +0100

    fix: include nfs test recipe in policyfile

--- Policyfile.rb
+++ Policyfile.rb
@@ -2,7 +2,7 @@
 
 name 'filesystem'
 
-run_list 'test::default'
+run_list 'test::default', 'test::nfs'
 
 cookbook 'filesystem', path: '.'
 cookbook 'lvm', git: 'https://github.com/sous-chefs/lvm.git', branch: 'main'
@@ -10,6 +10,9 @@ cookbook 'test', path: './test/cookbooks/test'
 
 Dir.children('./test/cookbooks/test/recipes').grep(/\.rb\z/).sort.each do |recipe|
   recipe_name = File.basename(recipe, '.rb')
+  next if recipe_name == 'default'
 
   named_run_list recipe_name.to_sym, 'test::' + recipe_name
 end
+
+named_run_list :default, 'test::default', 'test::nfs'
